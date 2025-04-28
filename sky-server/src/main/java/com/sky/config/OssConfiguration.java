8c02855db03841bf8648e894a2576973a4857eb4
致爱蓝海 <1595711067@qq.com>
2025-04-28 23:22:31 +0800

    完成通用接口文件上传，使用 AliOssUtil 工具类将文件上传到阿里云 OSS

---
+++ sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -0,0 +1,27 @@
+package com.sky.config;
+
+import com.sky.properties.AliOssProperties;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置类，用于创建AliOssUtil
+ */
+@Configuration
+@Slf4j
+public class OssConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    //@ConditionalOnMissingBean 注解用于在 Spring 容器中，当没有指定类型的 Bean 存在时，才创建并注册带有此注解的 Bean
+    public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties) {
+        log.info("开始创建阿里云文件上传工具类对象:{}", aliOssProperties);
+        return new AliOssUtil(aliOssProperties.getEndpoint(),
+                aliOssProperties.getAccessKeyId(),
+                aliOssProperties.getAccessKeySecret(),
+                aliOssProperties.getBucketName());
+    }
+}
