2e77f8315703794b283a9583d5312a017acd8346
致爱蓝海 <1595711067@qq.com>
2025-05-02 20:30:51 +0800

    优化菜品批量删除及口味数据关联处理

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -22,4 +22,11 @@ public interface DishFlavorMapper {
      */
     @Delete("delete from dish_flavor where dish_id =#{dishId}")
     void deleteByDishId(Long DishId);
+
+    /**
+     * 根据菜品id集合批量删除口味数据
+     *
+     * @param dishIds
+     */
+    void deleteByDishIds(List<Long> dishIds);
 }
