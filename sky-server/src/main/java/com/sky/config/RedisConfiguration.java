5879767208e97effa6a11d536b7cd26b94f50d1b
致爱蓝海 <1595711067@qq.com>
2025-05-12 23:48:40 +0800

    添加微信登录功能及用户端接口

--- sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -12,9 +12,9 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfiguration {
 
     @Bean
-    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory){
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         log.info("开始创建redis模版对象...");
-        RedisTemplate redisTemplate =new RedisTemplate();
+        RedisTemplate redisTemplate = new RedisTemplate();
         //设置redis的连接工厂对象
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         //设置redis key的序列化器
