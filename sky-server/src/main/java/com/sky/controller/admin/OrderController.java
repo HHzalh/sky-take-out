82a31da4b02a6d9c31334ff49e04c9f1e467cd43
致爱蓝海 <1595711067@qq.com>
2025-06-03 15:55:59 +0800

    完成各个状态的订单数量统计业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -4,6 +4,7 @@ import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
+import com.sky.vo.OrderStatisticsVO;
 import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -50,5 +51,17 @@ public class OrderController {
     public Result<OrderVO> details(@PathVariable Long id) {
         OrderVO orderVO = orderService.getOrdersDetailById(id);
         return Result.success(orderVO);
+    }
+
+    /**
+     * 各个状态的订单数量统计
+     *
+     * @return
+     */
+    @ApiOperation("各个状态的订单数量统计")
+    @GetMapping("/statistics")
+    public Result<OrderStatisticsVO> statistics() {
+        OrderStatisticsVO orderStatisticsVO = orderService.statistics();
+        return Result.success(orderStatisticsVO);
     }
 }
